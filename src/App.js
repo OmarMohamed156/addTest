681a62d3dc26b2c2ff7e572e93e5217438eef3e1
OmarMohamed156 <om658036@gmail.com>
2022-07-13 21:14:54 +0200

    adjusted add &remove logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React, { useState }  from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FormGroup,Label,Input,Form,Button, Row, Col } from 'reactstrap';
@@ -6,162 +6,181 @@ import { FaPaperPlane, FaPlus , FaTrash} from 'react-icons/fa';
 
 
 function App() {
-  const [inputFields, setInputFields] = useState([
+  const [scores,setScores] = useState([{
+    min:'',
+    max:'',
+    result:{en:'',ar:''},
+    description:{en:'',ar:''}
+  }])
+  const [inputFields,setInputFields] = useState(
     {
       description: {ar:'',en:''},
       title: {ar:'',en:''},
       category:'',
       rules:{ar:'',en:''},
-    }])
-
-  const [questions,setQuestions] = useState([
-    {
-      body:{ar:'',en:''},
+      questions:[
+        {
+          body:{ar:'',en:''},
+          answers:[
+            {
+              body:{ar:'',en:''},
+              weight:''
+            }
+          ]
+        },
+      ]
     }
-  ])
+  )
 
-  const [answers,setAnswers] = useState([
-    {
-      body:{ar:'',en:''}
-    }
-  ])
 
   const addQuestion = () =>{
-    setQuestions([...questions,{
+    const values = {...inputFields}
+    values.questions.push({
       body:{ar:'',en:''},
-      type:'',
-    }])
+      answers:[
+        {
+          body:{ar:'',en:''},
+          weight:''
+        }
+      ]
+    })
+    setInputFields(values)
   }
-  const removeQuestion = (index) =>{
-    const values =[...questions]
-    if(values.length === 1){
+
+  const addAnswer = (qIndex) =>{
+    const values = {...inputFields}
+    values.questions[qIndex].answers.push(
+      {
+        body:{ar:'',en:''},
+        weight:''
+      }
+    )
+    setInputFields(values)
+  }
+
+  const removeQuestion =(qindex)=>{
+    const values ={...inputFields}
+    if (values.questions.length === 1){
       return
     }
     else{
-      values.splice(index,1)
-      setQuestions(values)
+      values.questions.splice(qindex,1)
+      setInputFields(values)
     }
   }
 
-  const addAnswer = () =>{
-    setAnswers([...answers,    {
-      body:{ar:'',en:''}
-    }])
-  }
-
-  const removeAnswer = (index) =>{
-    const values = [...answers]
-    if(values.length === 1){
+  const removeAnswer = (qIndex,aIndex)=>{
+    const values = {...inputFields}
+    if(values.questions[qIndex].answers.length === 1){
       return
     }
     else{
-      values.splice(index,1)
-      setAnswers(values)
+      values.questions[qIndex].answers.splice(aIndex,1)
+      setInputFields(values)
     }
   }
 
-  const handleChangeQuestion =(index,e)=>{
-    const values = [...questions]
-    switch (e.target.name){
-      case 'body-ar':
-        values[index].body.ar = e.target.value
-        break
-      case 'body-en':
-        values[index].body.en = e.target.value
-        break
-      default:
-        values[index][e.target.name] = e.target.value
-    }
-    setQuestions(values)
-  }
+  // const handleChangeQuestion =(index,e)=>{
+  //   const values = [...questions]
+  //   switch (e.target.name){
+  //     case 'body-ar':
+  //       values[index].body.ar = e.target.value
+  //       break
+  //     case 'body-en':
+  //       values[index].body.en = e.target.value
+  //       break
+  //     default:
+  //       values[index][e.target.name] = e.target.value
+  //   }
+  //   setQuestions(values)
+  // }
 
   
-  const handleAnswerChange =(index,e)=>{
-    const values = [...answers]
-    switch (e.target.name){
-      case 'answer-ar':
-        values[index].body.ar = e.target.value
-        break
-      case 'answer-en':
-        values[index].body.en = e.target.value
-        break
-      default:
-        values[index][e.target.name] = e.target.value
-    }
-    values[index][e.target.name] = e.target.value
-    setAnswers(values)
+  // const handleAnswerChange =(index,e)=>{
+  //   const values = [...answers]
+  //   switch (e.target.name){
+  //     case 'answer-ar':
+  //       values[index].body.ar = e.target.value
+  //       break
+  //     case 'answer-en':
+  //       values[index].body.en = e.target.value
+  //       break
+  //     default:
+  //       values[index][e.target.name] = e.target.value
+  //   }
+  //   values[index][e.target.name] = e.target.value
+  //   setAnswers(values)
     
-  }
+  // }
   const handleInputFieldChange =(index,e)=>{
-    const values = [...inputFields]
+    const values = {...inputFields}
     switch (e.target.name){
       case 'title-ar':
-        values[index].title.ar = e.target.value
+        values.title.ar = e.target.value
         break;
       case 'title-en':
-        values[index].title.en = e.target.value
+        values.title.en = e.target.value
         break;
       case 'description-ar':
-        values[index].description.ar = e.target.value
+        values.description.ar = e.target.value
         break;
       case 'description-en':
-        values[index].description.en = e.target.value
+        values.description.en = e.target.value
         break
       case 'rules-ar':
-        values[index].rules.ar = e.target.value
+        values.rules.ar = e.target.value
         break
       case 'rules-en':
-        values[index].rules.en = e.target.value
+        values.rules.en = e.target.value
         break
       default:
-        values[index][e.target.name] = e.target.value
+        values[e.target.name] = e.target.value
     }
     setInputFields(values)
   }
 
 
 
-    const handleSubmit = (e) =>{
-    e.preventDefault();
-    let test ={}
-    test.title = inputFields[0].title
-    test.category = inputFields[0].category
-    test.description = inputFields[0].description
-    test.rules = inputFields[0].rules
-    // for(let i = 0; i< questions.length ;i++){
-    //   questions[i].answers = 'hello'
-    //   console.log(inputFields[i])
-    // }
-    console.log(inputFields)
-    console.log(questions)
-    console.log(answers)
-    // console.log(questions)
-    // test.questions = questions
-    // console.log(test)
-    // const form = document.getElementById('testForm')
-    // form.reset()
-  }
+  //   const handleSubmit = (e) =>{
+  //   e.preventDefault();
+  //   let test ={}
+  //   test.title = inputFields[0].title
+  //   test.category = inputFields[0].category
+  //   test.description = inputFields[0].description
+  //   test.rules = inputFields[0].rules
+  //   // for(let i = 0; i< questions.length ;i++){
+  //   //   questions[i].answers = 'hello'
+  //   //   console.log(inputFields[i])
+  //   // }
+  //   console.log(inputFields)
+  //   console.log(questions)
+  //   console.log(answers)
+  //   // console.log(questions)
+  //   // test.questions = questions
+  //   // console.log(test)
+  //   // const form = document.getElementById('testForm')
+  //   // form.reset()
+  // }
 
 
   return (
-    <Form id='testForm' className='px-4 my-3' onSubmit={handleSubmit}>
-      {inputFields.map((field,index)=>(
+    <Form id='testForm' className='px-4 my-3' >
         <>
-        <FormGroup  key={index}>
+        <FormGroup>
           <Row className='my-2'>
             <Col sm={12} md={1}>
               <Label className='mt-2'>Title in Arabic</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input onChange={e=>{handleInputFieldChange(index,e)}}  placeholder='title-ar' type='text' name='title-ar'/>
+              <Input  placeholder='title-ar' type='text' name='title-ar'/>
             </Col>
           </Row>
           <Row>
             <Col sm={12} md={1}>
-              <Label onChange={e=>{handleInputFieldChange(index,e)}} className='mt-2'>Title in English</Label>
+              <Label className='mt-2'>Title in English</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input onChange={e=>{handleInputFieldChange(index,e)}} placeholder='title-en' type='text' name='title-en'/>
+              <Input  placeholder='title-en' type='text' name='title-en'/>
             </Col>
           </Row>
           <Row className='my-2'>
@@ -169,7 +188,7 @@ function App() {
               <Label className='mt-2'>Description in Arabic</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input placeholder='description-ar'  type='text' onChange={e=>{handleInputFieldChange(index,e)}} name='description-ar' />
+              <Input placeholder='description-ar'  type='text'  name='description-ar' />
             </Col>
           </Row>
           <Row className='my-2'>
@@ -177,7 +196,7 @@ function App() {
               <Label className='mt-2'>Description in English</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input onChange={e=>{handleInputFieldChange(index,e)}} placeholder='description-en' type='text' name='description-en'/>
+              <Input  placeholder='description-en' type='text' name='description-en'/>
             </Col>
           </Row>        
           <Row className='my-2'>
@@ -185,7 +204,7 @@ function App() {
               <Label className='mt-2'>Category</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input onChange={e=>{handleInputFieldChange(index,e)}} placeholder='category' type='text' name='category'  />
+              <Input placeholder='category' type='text' name='category'  />
             </Col>
           </Row>
           <Row className='my-2'>
@@ -193,7 +212,7 @@ function App() {
               <Label className='mt-2'>Rules in Arabic</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input  onChange={e=>{handleInputFieldChange(index,e)}} type='textarea' name='rules-ar' placeholder='rules-ar'  />
+              <Input   type='textarea' name='rules-ar' placeholder='rules-ar'  />
             </Col>
           </Row>
           <Row className='my-2'>
@@ -201,20 +220,17 @@ function App() {
               <Label className='mt-2'>Rules in English</Label>
             </Col>
             <Col sm={12} md={11}>
-              <Input  onChange={e=>{handleInputFieldChange(index,e)}} type='textarea' name='rules-en' placeholder='rules-en'  />
+              <Input  type='textarea' name='rules-en' placeholder='rules-en'  />
             </Col>
           </Row>
-        </FormGroup>
-        <FormGroup >
-        {questions.map((question,i)=>(  
+          {inputFields.questions.map((question,i)=>(  
           <div key={i} className='border p-4 rounded border-2 border-dark my-3'>
-            
             <Row className='my-2'>
               <Col sm={12} md={1}>
                 <Label className='mt-2'>Body in Arabic</Label>
               </Col>
               <Col sm={12} md={11}>
-                <Input onChange={e=>{handleChangeQuestion(i,e)}} placeholder='body-ar' type='text' name='body-ar'  />
+                <Input placeholder='body-ar' type='text' name='body-ar'  />
               </Col>
             </Row>
             <Row className='my-2'>
@@ -222,50 +238,46 @@ function App() {
                 <Label className='mt-2'>Body in English</Label>
               </Col>
               <Col sm={12} md={11}>
-                <Input onChange={e=>{handleChangeQuestion(i,e)}} placeholder='body-en' type='text' name='body-en'  />
+                <Input placeholder='body-en' type='text' name='body-en'  />
               </Col>
             </Row>
-            {/* <Row className='my-2'>
-              <Col sm={12} md={1}>
-                <Label className='mt-2'>Type</Label>
-              </Col>
-              <Col sm={12} md={11}>
-                <Input onChange={e=>{handleChangeQuestion(i,e)}} placeholder='type' type='text' name='type' />
-              </Col>
-            </Row> */}
-            {answers.map((answer,j)=>(
+            <h5>Answer(s) for question no {i+1}</h5>
+            {question.answers.map((answer,j)=>(
               <>
-                <div>Answers for the above question</div>
                 <div key={j} className='border p-4 rounded border-2 border-dark my-3'>
                   <Row className='my-2'>
                     <Col sm={12} md={1}>
                       <Label className='mt-2'>Answer in Arabic</Label>
                     </Col>
                     <Col sm={12} md={11}>
-                      <Input onChange={e=>{handleAnswerChange(j,e)}} placeholder='answer-ar' type='text' name='answer-ar'  />
+                      <Input placeholder='answer-ar' type='text' name='answer-ar'  />
                     </Col>
                   </Row>
                   <Row className='my-2'>
                     <Col sm={12} md={1}>
                       <Label className='mt-2'>Answer in English</Label>
                     </Col>
-                    <Col sm={12} md={11}>
-                      <Input onChange={e=>{handleAnswerChange(j,e)}} placeholder='answer-en' type='text' name='answer-en'/>
+                    <Col sm={12} md={6}>
+                      <Input  placeholder='answer-en' type='text' name='answer-en'/>
+                    </Col>
+                    <Col sm={12} md={5}>
+                      <Input  placeholder='Answer Weight' type='number' name='weight'/>
                     </Col>
                   </Row>
-                  <Button className='mt-3 mx-2 btn-success'  onClick={()=>{addAnswer()}}>Add Answer <FaPlus/></Button>
-                  <Button className='mt-3 mx-2 btn-danger' onClick={()=>{removeAnswer(j)}}>Delete Answer <FaTrash/></Button>
+                  <Button className='mt-3 mx-2 btn-success' onClick={()=>{addAnswer(i)}} >Add Answer <FaPlus/></Button>
+                  <Button className='mt-3 mx-2 btn-danger' onClick={()=>{removeAnswer(i,j)}} >Delete Answer <FaTrash/></Button>
                 </div>
               </>
             ))}
-            <Button onClick={()=>{addQuestion()}} className='mt-3 mx-2 btn-success'>Add new question <FaPlus/></Button>
-            <Button className='mt-3 mx-2 btn-danger' onClick={()=>{removeQuestion(i)}}>Delete question  <FaTrash/></Button>
+            <Button  className='mt-3 mx-2 btn-success' onClick={()=>{addQuestion()}} >Add new question<FaPlus/></Button>
+            <Button className='mt-3 mx-2 btn-danger' onClick={()=>{removeQuestion(i)}} >Delete question<FaTrash/></Button>
             </div>
         ))
         }
         </FormGroup>
+        <FormGroup >
+        </FormGroup>
         </>
-      ))}
       <Button type='submit'>Submit <FaPaperPlane/></Button>
     </Form>
   );
